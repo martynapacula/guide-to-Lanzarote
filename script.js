04af9b3405a83ac509b392367d50635b9895e775
martynapacula <martynapacula@gmail.com>
2019-11-10 18:30:57 +0000

    map marker added

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
+          center: {lat: 29.0603779, lng: -13.6740699},
+          zoom: 10
         });
       }
+
+       var marker = new google.maps.Marker({
+    position: {Lat:29.1579997  ,Lng:-13.441186  },
+    map: map,
+
+  });
+}
